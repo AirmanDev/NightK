5c7d6acbb38123a842835b979487f861bd704494
AirmanDev <airmancooma@gmail.com>
2026-06-09 20:24:10 +0200

    Initial Commit

---
+++ NightK/NightKApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightKApp.swift
+//  NightK
+//
+//  Created by Airmancooma on 2026. 06. 09..
+//
+
+import SwiftUI
+
+@main
+struct NightKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
